536deeae7e57d85d8c1e1101bf5e31089c6b8025
Onur Kiroglu <onur.kiroglu@vendrops.com>
2022-08-11 23:02:42 +0300

    static files loaded

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
-const fn = function () {
-    console.log('Function working')
-}
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const myLogger = (req, res, next) => {
+  console.log("Middleware Log 1");
+  next();
+};
+
+const myLogger2 = (req, res, next) => {
+  console.log("Middleware Log 2");
+  next();
+};
+
+//MIDDLEWARES
+app.use(express.static("public"));
+app.use(myLogger);
+app.use(myLogger2);
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./temp/index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`sunucu ${port}'unda başlatıldı`);
+});
